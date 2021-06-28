5d33a5532aea8402eb5956b40320c428d981d3e7
waterfallm <matthew@waterfall-family.co.uk>
2021-06-28 03:04:56 +0100

    Update useCakes.ts

--- src/queries/useCakes.ts
+++ src/queries/useCakes.ts
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 // import { IAddress } from '../types';
 
-const baseurl = "http://home1.ball-coin.com"
+const baseurl = "https://home1.ball-coin.com"
 
 export const getCakes = async () => {
   // const result = await axios.get(`/api/addresses/${id}`);
